6a472da27358c6b197bbb2a0e3f9e4a8ed6f9d4b
Jacob S <jacob@gqc.com>
2019-09-12 13:22:24 -0400

    Added projects module (empty) and modified the substances models.

---
+++ traci/projects/models.py
@@ -0,0 +1,21 @@
+# models.py (projects)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+Models for Projects (LCI Data).
+
+Available models:
+- None yet
+"""
+
+from django.db import models
+
+
+class Project(models.Model):
+    """Project object contains LCI Data."""
+    #name
+    #version
+    #organization
+    #
--- traci/substances/models.py
+++ traci/substances/models.py
@@ -10,6 +10,7 @@ Available models:
 - Substance
 - Ecotox
 - HHCF
+- FossilFuel
 """
 
 from django.db import models
@@ -34,7 +35,7 @@ class Substance(models.Model):
 
 
 class Ecotox(models.Model):
-    """TODO."""
+    """Information related to a chemical's ecotoxicity potential."""
     #id = models.AutoField(primary_key=True)
     ecotox_airu = models.FloatField(blank=False, null=False, default=0)
     ecotox_airc = models.FloatField(blank=False, null=False, default=0)
@@ -42,14 +43,18 @@ class Ecotox(models.Model):
     ecotox_seawaterc = models.FloatField(blank=False, null=False, default=0)
     ecotox_nat_soilc = models.FloatField(blank=False, null=False, default=0)
     ecotox_agr_soilc = models.FloatField(blank=False, null=False, default=0)
-    ecotox_flag = models.CharField(max_length=12, choices=TRACI_FLAG_OPTIONS, default=DEFAULT_TRACI_FLAG)
+    ecotox_flag = models.CharField(max_length=12,
+                                   choices=TRACI_FLAG_OPTIONS,
+                                   default=DEFAULT_TRACI_FLAG)
 
 
 class Hhcf(models.Model):
-    """TODO."""
+    """Information related to a chemical's Human Health Characterization Factor."""
     #id = models.AutoField(primary_key=True)
-    hhcf_air_cancer = models.FloatField(blank=False, null=False, default=0)
-    hhcf_air_noncanc = models.FloatField(blank=False, null=False, default=0)
+    hhcf_urban_air_cancer = models.FloatField(blank=False, null=False, default=0)
+    hhcf_urban_air_noncanc = models.FloatField(blank=False, null=False, default=0)
+    hhcf_rural_air_cancer = models.FloatField(blank=False, null=False, default=0)
+    hhcf_rural_air_noncanc = models.FloatField(blank=False, null=False, default=0)
     hhcf_freshwater_cancer = models.FloatField(blank=False, null=False, default=0)
     hhcf_freshwater_noncanc = models.FloatField(blank=False, null=False, default=0)
     hhcf_seawater_cancer = models.FloatField(blank=False, null=False, default=0)
@@ -58,11 +63,15 @@ class Hhcf(models.Model):
     hhcf_natsoil_noncanc = models.FloatField(blank=False, null=False, default=0)
     hhcf_agrsoil_cancer = models.FloatField(blank=False, null=False, default=0)
     hhcf_agrsoil_noncanc = models.FloatField(blank=False, null=False, default=0)
-    hhcf_carcinogenic_flag = models.CharField(max_length=12, choices=TRACI_FLAG_OPTIONS, default=DEFAULT_TRACI_FLAG)
-    hhcf_noncarcinogenic_flag = models.CharField(max_length=12, choices=TRACI_FLAG_OPTIONS, default=DEFAULT_TRACI_FLAG)
+    hhcf_carcinogenic_flag = models.CharField(max_length=12,
+                                              choices=TRACI_FLAG_OPTIONS,
+                                              default=DEFAULT_TRACI_FLAG)
+    hhcf_noncarcinogenic_flag = models.CharField(max_length=12,
+                                                 choices=TRACI_FLAG_OPTIONS,
+                                                 default=DEFAULT_TRACI_FLAG)
 
 
-class FossilFuels(models.Model):
-    """Contains information related to Fossil Fuel CF (MJ/MJ) for specific Fossil Fuels Consumed."""
+class FossilFuel(models.Model):
+    """Information related to Fossil Fuel CF (MJ/MJ) for specific Fossil Fuels Consumed."""
     name = models.CharField(blank=False, null=False, max_length=255)
     cf = models.FloatField(blank=False, null=False, default=0)
