97f1c9df72c62f9b39f5184ed9c32bfda4a17197
Daniel L. Young, Ph.D <young.daniel@epa.gov>
2019-09-18 09:10:47 -0400

    More PEP8/257 fixes, updated requitements.txt

--- traci/substances/admin.py
+++ traci/substances/admin.py
@@ -4,6 +4,16 @@
 # young.daniel@epa.gov
 
 
+"""
+Defines classes used to generate Django Admin portion of website.
+
+Should be an Admin class for each Model that can be modified by an admin user.
+
+Available functions:
+- TBD
+"""
+
+
 from django.contrib import admin
 
 # Register your models here.
