8fe0c7d51db11ec11517c7a1512eab505a7f3f62
Jacob S <jacob@gqc.com>
2019-09-25 15:11:33 -0400

    Removed extraneous static files and performed major restructuring and upgrades of static dependencies.

--- traci/traci/settings.py
+++ traci/traci/settings.py
@@ -154,7 +154,9 @@ else:
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+# STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = os.path.join(BASE_DIR, 'traci', 'static')
+
 
 ########################################################################
 DOWNLOADS_DIR = os.path.join("..", "DOCS")
