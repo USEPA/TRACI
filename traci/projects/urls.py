922a3fb06c5249a3c60c2da7d627d873c9ad851b
Jacob S <jacob@gqc.com>
2019-09-27 13:38:27 -0400

    Modified URLs so ending slash / is optional.

--- traci/projects/urls.py
+++ traci/projects/urls.py
@@ -17,10 +17,10 @@ from projects.views import ProjectListView, ProjectCreateView, get_projects, Pro
     ProjectDetailView, ProjectDeleteView
 
 urlpatterns = [
-    url(r'^projects/$', ProjectListView.as_view(), name='projects'),
-    url(r'^create/$', ProjectCreateView.as_view(), name='create_project'),
-    url(r'^edit/(?P<pk>\d+)/$', ProjectEditView.as_view(), name='edit_project'),
-    url(r'^detail/(?P<pk>\d+)/$', ProjectDetailView.as_view(), name='detail_project'),
-    url(r'^delete/(?P<pk>\d+)/$', ProjectDeleteView.as_view(), name='delete_project'),
-    url(r'^get_projects/$', get_projects, name='get_projects'),
+    url(r'^projects/?$', ProjectListView.as_view(), name='projects'),
+    url(r'^create/?$', ProjectCreateView.as_view(), name='create_project'),
+    url(r'^edit/(?P<pk>\d+)/?$', ProjectEditView.as_view(), name='edit_project'),
+    url(r'^detail/(?P<pk>\d+)/?$', ProjectDetailView.as_view(), name='detail_project'),
+    url(r'^delete/(?P<pk>\d+)/?$', ProjectDeleteView.as_view(), name='delete_project'),
+    url(r'^get_projects/?$', get_projects, name='get_projects'),
 ]
