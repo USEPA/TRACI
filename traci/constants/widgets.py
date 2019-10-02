0381c153ff1b19198dae02eb0b54e40ab97211a5
Jacob S <jacob@gqc.com>
2019-10-02 15:28:16 -0400

    About to clear all migrations and test with a fresh database.

---
+++ traci/constants/widgets.py
@@ -0,0 +1,30 @@
+# widgets.py (constants)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+Defines custom widgets to be shared across any EPA Django apps.
+
+Available widgets:
+- ListTextWidget allows the user to select from a dropdown or optionally enter an option as free text.
+"""
+
+from django import forms
+
+# https://stackoverflow.com/questions/24783275/django-form-with-choices-but-also-with-freetext-option
+class ListTextWidget(forms.TextInput):
+    def __init__(self, data_list, name, *args, **kwargs):
+        super(ListTextWidget, self).__init__(*args, **kwargs)
+        self._name = name
+        self._list = data_list
+        self.attrs.update({'list':'list__%s' % self._name})
+
+    def render(self, name, value, attrs=None, renderer=None):
+        text_html = super(ListTextWidget, self).render(name, value, attrs=attrs)
+        data_list = '<datalist id="list__%s">' % self._name
+        for item in self._list:
+            data_list += '<option value="%s">' % item
+        data_list += '</datalist>'
+
+        return (text_html + data_list)
--- traci/products/urls.py
+++ traci/products/urls.py
@@ -15,7 +15,7 @@ Available functions:
 from django.conf.urls import url
 from products.views import ProductCreateView, ProductEditView, ProductDetailView, ProductDeleteView, \
     LifeCycleStageEntryCreateView, LifeCycleStageEntryEditView, LifeCycleStageEntryDetailView, \
-    LifeCycleStageEntryDeleteView
+    LifeCycleStageEntryDeleteView, ProcessCreateView, ProcessEditView, ProcessDetailView, ProcessDeleteView
 
 urlpatterns = [
     # URLs for Product CRUD operations.
@@ -24,9 +24,15 @@ urlpatterns = [
     url(r'^detail/(?P<pk>\d+)/?$', ProductDetailView.as_view(), name='detail_product'),
     url(r'^delete/(?P<pk>\d+)/?$', ProductDeleteView.as_view(), name='delete_product'),
 
-    # URLs for LifeCycleStage CRUD operations.
+    # URLs for LifeCycleStageName CRUD operations.
     url(r'^lifecyclestage/create/?$', LifeCycleStageEntryCreateView.as_view(), name='lifecyclestage_create'),
     url(r'^lifecyclestage/edit/(?P<pk>\d+)/?$', LifeCycleStageEntryEditView.as_view(), name='edit_lifecyclestage'),
     url(r'^lifecyclestage/detail/(?P<pk>\d+)/?$', LifeCycleStageEntryDetailView.as_view(), name='detail_lifecyclestage'),
     url(r'^lifecyclestage/delete/(?P<pk>\d+)/?$', LifeCycleStageEntryDeleteView.as_view(), name='delete_lifecyclestage'),
+
+    # URLs for Process CRUD operations.
+    url(r'^process/create/?$', ProcessCreateView.as_view(), name='lifecyclestage_create'),
+    url(r'^process/edit/(?P<pk>\d+)/?$', ProcessEditView.as_view(), name='edit_lifecyclestage'),
+    url(r'^process/detail/(?P<pk>\d+)/?$', ProcessDetailView.as_view(), name='detail_lifecyclestage'),
+    url(r'^process/delete/(?P<pk>\d+)/?$', ProcessDeleteView.as_view(), name='delete_lifecyclestage'),
 ]
