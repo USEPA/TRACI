e5e9b47e70f0c0094dbafbbd3c0f13d982f2a06e
Jacob S <jacob@gqc.com>
2019-10-03 15:31:02 -0400

    About to flush migrations again.

--- traci/products/migrations/0003_auto_20191003_0959.py
+++ traci/products/migrations/0003_auto_20191003_0959.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('products', '0002_auto_20191002_1537'),
+        ('products', '0002_loaddata'),
     ]
 
     operations = [
--- traci/substances/migrations/0003_unit.py
+++ traci/substances/migrations/0003_unit.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('substances', '0002_auto_20191002_1538'),
+        ('substances', '0002_loaddata'),
     ]
 
     operations = [
