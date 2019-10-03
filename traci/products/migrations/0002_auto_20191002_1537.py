adfde1cc846a6f2f363167a36af0a042868882c8
Jacob S <jacob@gqc.com>
2019-10-03 12:03:55 -0400

    Two more fixtures added with data from old traci tool. Migration upgraded to apply these fixtures for fresh instances.

--- traci/products/migrations/0002_auto_20191002_1537.py
+++ traci/products/migrations/0002_auto_20191002_1537.py
@@ -9,15 +9,27 @@ from django.db.utils import IntegrityError
 fixture_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../fixtures'))
 fixture_lifecyclestagename = 'lifecyclestagename.json'
 fixture_location = 'location.json'
+fixture_resourcereleasetype = 'resourcereleasetype.json'
+fixture_media = 'media.json'
 
 def load_lifecyclestagenames(_apps, _schema_editor):
     """Load data from lifecyclestagenames fixture when running migrations"""
     load_fixture(_apps, _schema_editor, fixture_lifecyclestagename)
-
+    
 
 def load_locations(_apps, _schema_editor):
     """Load data from locations fixture when running migrations"""
     load_fixture(_apps, _schema_editor, fixture_location)
+
+
+def load_resourcereleasetypes(_apps, _schema_editor):
+    """Load data from resourcereleasetypes fixture when running migrations"""
+    load_fixture(_apps, _schema_editor, fixture_resourcereleasetype)
+    
+
+def load_media(_apps, _schema_editor):
+    """Load data from media fixture when running migrations"""
+    load_fixture(_apps, _schema_editor, fixture_media)
 
 
 def load_fixture(_apps, _schema_editor, filename):
@@ -43,4 +55,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunPython(load_lifecyclestagenames),
         migrations.RunPython(load_locations),
+        migrations.RunPython(load_resourcereleasetypes),
+        migrations.RunPython(load_media),
     ]
