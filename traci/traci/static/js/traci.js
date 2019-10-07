cf6debcb891f5223eb4d3c01854a95855001c048
Jacob S <jacob@gqc.com>
2019-10-07 10:27:38 -0400

    Resource/Release create new, edit, delete are functioning. Looking into factors now. Will begin calculation functionality soon.

--- traci/traci/static/js/traci.js
+++ traci/traci/static/js/traci.js
@@ -1,4 +1,28 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
 function fromEditToDetail() {
     var path = window.location.pathname.replace('edit', 'detail');
     window.location.href = path;
+}
+
+function releaseResourceCancel() {
+    var id = getUrlParameter('process_id');
+    if (!id) {
+        history.back();
+        return;
+    }
+    window.location.href = '/products/process/detail/' + id;
 }
